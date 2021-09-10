89b50a7457b961632d53b8bc25580c9cf36618f3
kewisel910 <kewisel910@stvbz.com>
2021-09-10 14:48:23 +0200

    add network details

--- docker-details.sh
+++ docker-details.sh
@@ -1,14 +1,21 @@
 DIR=docker-report
 HISTORY_DIR=$DIR"/history"
 INSPECT_DIR=$DIR"/inspect"
-mkdir $DIR $HISTORY_DIR $INSPECT_DIR
+NETWORK_DIR=$DIR"/network"
+mkdir $DIR $HISTORY_DIR $INSPECT_DIR $NETWORK_DIR
 
 
 echo "=====================DOCKER INFO=========================" >> $DIR/docker.info
 docker info >> $DIR/docker.info
 
+echo "=====================DOCKER CONFIG=======================" >> $DIR/docker.config
+docker config ls >> $DIR/docker.config
+
 echo "=====================DOCKER CONTAINERS===================" >> $DIR/docker.containers
 docker ps -a >> $DIR/docker.containers
+
+echo "=====================DOCKER NETWORKS=====================" >> $DIR/docker.networks
+docker network ls >> $DIR/docker.networks
 
 echo "=====================DOCKER IMAGES=======================" >> $DIR/docker.images
 docker image ls >> $DIR/docker.images
@@ -31,6 +38,12 @@ do
 	echo `docker port $container` >> $DIR/docker.ports
 done
 
+# DOCKER NETWORK INSPECT
+echo "=====================DOCKER NETWORK INSPECT============" >> $NETWORK_DIR
+for net in `docker network ls | awk 'NR>1 {print $2}'`
+do
+	echo "========${net}========" >> $NETWORK_DIR/${net}.json
+	echo `docker network inspect ${net}` >> $NETWORK_DIR/${net}.json
+done
+
 tar cvzf docker-report.tar.gz docker-report/
-
-
